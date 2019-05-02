a577b28b4ff06a94354c6154fdc8ab0225f7384a
cpmmarcelo <marcelo@cpmnet.com.br>
2019-05-02 13:00:41 -0300

    Update JavaApplication5.java

--- src/javaapplication5/JavaApplication5.java
+++ src/javaapplication5/JavaApplication5.java
@@ -17,7 +17,7 @@ public class JavaApplication5 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Testando o github");
+        System.out.println("Testando o github Alterado");
     }
     
 }
